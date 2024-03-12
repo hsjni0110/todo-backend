46476d82c233c6db7ea340da0c7d0e66e9de4bb5
hsjni0110 <hansukjinid@gmail.com>
2024-03-12 19:23:26 +0900

    feat: level1 clear

--- src/main/java/backend/likelion/todos/member/MemberRepository.java
+++ src/main/java/backend/likelion/todos/member/MemberRepository.java
@@ -3,6 +3,7 @@ package backend.likelion.todos.member;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import javax.swing.text.html.Option;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,25 +17,31 @@ public class MemberRepository {
         // TODO [1단계] member의 id를 설정하세요.
         // TODO [1단계] members 맵에 member를 추가하세요.
         // TODO [1단계] member를 반환하세요.
-        return null;
+        member.setId(id);;
+        members.put(id++, member);
+        return member;
     }
 
     // 주어진 id에 해당하는 멤버를 찾아 Optional로 반환합니다.
     public Optional<Member> findById(Long id) {
         // TODO [1단계] id를 이용하여 members 맵에서 멤버를 찾으세요.
         // TODO [1단계] 찾은 멤버를 Optional로 감싸서 반환하세요.
-        return null;
+        return Optional.ofNullable(members.get(id));
     }
 
     // 주어진 username과 일치하는 멤버를 찾아 Optional로 반환합니다.
     public Optional<Member> findByUsername(String username) {
         // TODO [1단계] members 맵에서 username이 일치하는 멤버를 스트림을 사용해 찾으세요.
         // TODO [1단계] 찾은 멤버를 Optional로 감싸서 반환하세요.
-        return null;
+        return members.values()
+                .stream()
+                .filter(it -> it.getUsername().equals(username))
+                .findAny();
     }
 
     // 저장소의 모든 멤버를 제거합니다.
     public void clear() {
         // TODO [1단계] members 맵의 모든 내용을 제거하세요.
+        members.clear();
     }
 }
