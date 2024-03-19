70b8b797fd992447d69bfab3fad2b5ca45a3db82
hsjni0110 <hansukjinid@gmail.com>
2024-03-19 20:11:12 +0900

    feat: level5 clear

--- src/main/java/backend/likelion/todos/auth/jwt/JwtService.java
+++ src/main/java/backend/likelion/todos/auth/jwt/JwtService.java
@@ -1,8 +1,5 @@
 package backend.likelion.todos.auth.jwt;
 
-import static java.util.concurrent.TimeUnit.DAYS;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-
 import backend.likelion.todos.common.UnAuthorizedException;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -17,8 +14,7 @@ public class JwtService {
     private final Algorithm algorithm;
 
     public JwtService(JwtProperty jwtProperty) {
-        this.accessTokenExpirationDayToMills =
-                MILLISECONDS.convert(jwtProperty.accessTokenExpirationDay(), DAYS);
+        this.accessTokenExpirationDayToMills = jwtProperty.accessTokenExpirationDay() * 24 * 60 * 60 * 1000;
         this.algorithm = Algorithm.HMAC512(jwtProperty.secretKey());
     }
 
@@ -27,12 +23,24 @@ public class JwtService {
         // TODO [5단계] 현재 시간과 설정된 만료 시간을 사용하여 만료 날짜를 설정하세요.
         // TODO [5단계] memberId를 클레임으로 추가하세요.
         // TODO [5단계] 설정된 알고리즘으로 토큰을 서명하고 반환하세요.
-        return null;
+        return JWT.create()
+                .withExpiresAt(new Date(accessTokenExpirationDayToMills + System.currentTimeMillis()))
+                .withIssuedAt(new Date())
+                .withClaim("memberId", memberId)
+                .sign(algorithm);
     }
 
     // 토큰에서 회원 ID를 추출합니다.
     public Long extractMemberId(String token) {
         // TODO [5단계] 알고리즘을 사용해 토큰의 유효성을 검증하고, "memberId" 클레임에서 회원 ID를 추출하세요. 유효하지 않은 경우 "유효하지 않은 토큰입니다." 메시지와 함께 UnAuthorizedException을 발생시키세요.
-        return null;
+        try {
+            return JWT.require(algorithm)
+                    .build()
+                    .verify(token)
+                    .getClaim("memberId")
+                    .asLong();
+        } catch (JWTVerificationException e) {
+            throw new UnAuthorizedException("유효하지 않은 토큰입니다.");
+        }
     }
 }
