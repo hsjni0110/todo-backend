e6bef9b95d943b12e48044334855cdbd8c69ec76
hsjni0110 <hansukjinid@gmail.com>
2024-04-01 16:33:45 +0900

    level6-clear

--- src/main/java/backend/likelion/todos/auth/AuthArgumentResolver.java
+++ src/main/java/backend/likelion/todos/auth/AuthArgumentResolver.java
@@ -2,7 +2,9 @@ package backend.likelion.todos.auth;
 
 import backend.likelion.todos.auth.jwt.JwtService;
 import backend.likelion.todos.common.UnAuthorizedException;
+import javax.security.sasl.AuthenticationException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -20,7 +22,7 @@ public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         // TODO [6단계] parameter가 @Auth 어노테이션을 갖고 있고, 파라미터 타입이 Long.class인 경우 true를 반환하는 조건을 구현하세요.
-        return false;
+        return parameter.hasParameterAnnotation(Auth.class) && parameter.getParameterType().equals(Long.class);
     }
 
     @Override
@@ -31,12 +33,20 @@ public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
             WebDataBinderFactory binderFactory
     ) {
         // TODO [6단계] webRequest로부터 accessToken을 추출하고, jwtService를 사용하여 memberId를 추출하여 반환하는 로직을 구현하세요.
-        return null;
+        Auth authAt = parameter.getParameterAnnotation(Auth.class);
+        String accessToken = extractAccessToken(webRequest);
+        return jwtService.extractMemberId(accessToken);
     }
 
     private static String extractAccessToken(NativeWebRequest request) {
         // TODO [6단계] request 헤더에서 "Authorization" 헤더 값을 추출하여 "Bearer "로 시작하는 accessToken을 반환하세요. 유효하지 않을 경우 "로그인 후 접근할 수 있습니다." 메시지와 함께 UnAuthorizedException을 발생시키는 로직을 구현하세요.
-        return null;
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+
+        throw new UnAuthorizedException("로그인 후 접근할 수 있습니다.");
     }
 
 }
